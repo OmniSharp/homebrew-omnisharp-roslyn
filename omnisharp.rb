3e0c4735232c5805c24c4c0225e9bb926861aa85
Simon Manning <simon@ecksd.com>
2015-09-09 11:40:50 +0200

    Updated formula to release 1.4.0

--- omnisharp.rb
+++ omnisharp.rb
@@ -1,9 +1,9 @@
 class Omnisharp < Formula
   desc "Cross platform .NET development in the editor of your choice"
   homepage "http://www.omnisharp.net/"
-  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.2.1/omnisharp.tar.gz"
-  version "1.2.1"
-  sha256 "3144caa1c777ec7398cc9d9042fc3ef62897a3da6ef1f2c228cf676f5c87cf27"
+  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.4.0/omnisharp.tar.gz"
+  version "1.4.0"
+  sha256 "be0a7ff48b8f1d6a36c5cedec1cb6d6fadc2a39a6d98b3cefaab1b3defde3904"
 
   depends_on "mono" => :recommended
   depends_on "aspnet/dnx/dnvm" => :recommended
