5c19c69d54e29bb9487caac22ef3f68dfadacd67
Simon Manning <simon@ecksd.com>
2017-04-06 15:39:45 +0100

    Added `omnisharp-mono` version.

---
+++ omnisharp-mono.rb
@@ -0,0 +1,25 @@
+class OmnisharpMono < Formula
+  desc "Cross platform .NET development in the editor of your choice"
+  homepage "http://www.omnisharp.net/"
+  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.13.0/omnisharp-mono.tar.gz"
+  version "1.13.0"
+  sha256 "4be4127ee170ad846637a94daff7e9a59ff2ced635793cd570e9e3b012f25c7a"
+  conflicts_with "omnisharp"
+
+  depends_on "mono" => :recommended
+
+  def install
+    libexec.install Dir["*"]
+
+    # To match non-mono install, create an `omnisharp' shell script.
+    (bin/"omnisharp").write <<-EOS.undent
+      #env /bin/sh
+      mono /usr/local/opt/omnisharp-mono/libexec/OmniSharp.exe $@
+    EOS
+  end
+
+  def post_install
+    system "/bin/chmod +x #{bin}/omnisharp"
+  end
+
+end
