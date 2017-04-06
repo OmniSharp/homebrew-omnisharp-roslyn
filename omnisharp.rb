a45cde72e576f10a28f88f4d326f0f682dc67a39
Simon Manning <simon@ecksd.com>
2017-04-06 14:09:56 +0100

    Updated version to 1.13.0.
    * Removed dependency on dnvm.

--- omnisharp.rb
+++ omnisharp.rb
@@ -1,12 +1,11 @@
 class Omnisharp < Formula
   desc "Cross platform .NET development in the editor of your choice"
   homepage "http://www.omnisharp.net/"
-  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.9-alpha13/omnisharp-osx-x64-netcoreapp1.0.tar.gz"
-  version "1.9-alpha13"
-  sha256 "7629d61f7d80581287584f197403f5cd4ff355a212b68430a314954a89ddf2b8"
+  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.13.0/omnisharp-osx-x64-netcoreapp1.1.tar.gz"
+  version "1.13.0"
+  sha256 "0238cfe4b2187bd3fb3c035574373d7d69dc2fe7fa1462f516c30a0b2008406d"
 
   depends_on "mono" => :recommended
-  depends_on "aspnet/dnx/dnvm" => :recommended
 
   def install
     libexec.install Dir["*"]
