91b9f260892bd9426e55a587e8a7a24a59552a29
Simon Manning <simon@ecksd.com>
2017-04-06 15:41:38 +0100

    Added conflicts_with on original formula.

--- omnisharp.rb
+++ omnisharp.rb
@@ -4,6 +4,7 @@ class Omnisharp < Formula
   url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.13.0/omnisharp-osx-x64-netcoreapp1.1.tar.gz"
   version "1.13.0"
   sha256 "0238cfe4b2187bd3fb3c035574373d7d69dc2fe7fa1462f516c30a0b2008406d"
+  conflicts_with "omnisharp-mono"
 
   depends_on "mono" => :recommended
 
