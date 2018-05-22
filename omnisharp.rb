2bc364bff5c236049a854e3881a1324bbdc993fc
Pieter Joost van de Sande <pj@born2code.net>
2018-05-22 09:58:45 +0200

    Update to v1.30.1

--- omnisharp.rb
+++ omnisharp.rb
@@ -1,9 +1,9 @@
 class Omnisharp < Formula
   desc "Cross platform .NET development in the editor of your choice"
   homepage "http://www.omnisharp.net/"
-  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.19.0/omnisharp-osx-x64-netcoreapp1.1.tar.gz"
-  version "1.19.0"
-  sha256 "6366dedb8fed547bb1cad40a2051c642287069793a22d7a3a3ba8cdbc0f932b4"
+  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.30.1/omnisharp-osx.tar.gz"
+  version "1.30.1"
+  sha256 "e2dfd016c1a6ea1dc4c96957c4be1c82d82769f33f56091f3082739421638bad"
   conflicts_with "omnisharp-mono"
 
   depends_on "mono" => :recommended
